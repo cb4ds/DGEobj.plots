715d3656257dc86c6023b8ecefc5d8ad51486926
saranya-ag <saranyakaruppasamy@ip-172-16-12-101.ca-central-1.compute.internal>
2021-03-02 05:11:42 -0800

    Refactored variables, added assert messages

--- R/plotPValHist.R
+++ R/plotPValHist.R
@@ -37,53 +37,88 @@ plotPvalHist <- function(P.Val,
                          color = "dodgerblue3",
                          fill = "dodgerblue3") {
 
+    assertthat::assert_that(!missing(P.Val),
+                            class(P.Val)[[1]] == "matrix" | class(P.Val)[[1]] == "data.frame",
+                            msg = "P.Val must be specified and must be of class matrix or dataframe.")
+
     assertthat::assert_that(plotType %in% c("canvasXpress", "ggplot"),
                             msg = "Plot type must be either canvasXpress or ggplot.")
+
+    if (!assertthat::see_if(is.character(color),
+                            length(color) == 1)) {
+        warning("color must be a singular value of class character and must specify the name of the color or the rgb value. Assigning default value 'dodgerblue3'.")
+        color <- "dodgerblue3"
+    }
+
+    if (!assertthat::see_if(is.character(fill))) {
+        warning("fill must be a singular value of class character and must specify the name of the color or the rgb value. Assigning default value 'dodgerblue3'.")
+        fill <- "dodgerblue3"
+    }
+
+    if (!assertthat::see_if(is.logical(facet),
+                            length(facet) == 1)) {
+        warning("facet must be a singular logical value. Assigning default value TRUE")
+        facet <- TRUE
+    }
+
+    if (!assertthat::see_if(is.numeric(binWidth),
+                            length(binWidth) == 1,
+                            binWidth > 0 & binWidth <= 1)) {
+        warning("binWidth must be a singular numeric value between 0 & 1. Assigning default value 0.02")
+        binWidth <- 0.02
+    }
+
+    if (!assertthat::see_if(is.numeric(alpha),
+                            alpha > 0 & alpha <= 1)) {
+        warning("symbolAlpha must be a singular numeric value and must be between 0 and 1. Assigning default value 0.3.")
+        alpha <- 0.3
+    }
 
     if (is.matrix(P.Val)) {
         P.Val <- P.Val %>%
             as.data.frame
     }
 
-    NumSamples <- ncol(P.Val)
-    SampNames <- colnames(P.Val)
+    samples_num <- ncol(P.Val)
+    samples_name <- colnames(P.Val)
 
     # Set up Tall format
+    browser()
     P.Val$GeneID = rownames(P.Val)
     P.Val <-  stats::reshape(data          = P.Val,
                              idvar         = "GeneID",
-                             varying       = SampNames,
-                             v.names       = "Pval",
+                             varying       = samples_name,
+                             v.names       = "pval",
                              direction     = "long",
-                             timevar       = "Levels",
-                             times         = SampNames,
-                             new.row.names = sequence(prod(length(SampNames), nrow(P.Val))))
+                             timevar       = "levels",
+                             times         = samples_name,
+                             new.row.names = sequence(prod(length(samples_name), nrow(P.Val))))
 
     title <- "P-value Histograms"
     plotlist <- list()
     if (plotType == "canvasXpress") {
         if (facet) {
-            cx.data <- subset(P.Val, select = Pval)
-            var.annot <- subset(P.Val, select = -c(Pval))
+            cx.data <- subset(P.Val, select = pval)
+            var.annot <- subset(P.Val, select = -c(pval))
             plotlist <- canvasXpress::canvasXpress(data                    = cx.data,
                                                    varAnnot                = var.annot,
                                                    histogramData           = TRUE,
                                                    graphType               = "Scatter2D",
-                                                   colors                  = color,
+                                                   colors                  = fill,
                                                    title                   = title,
                                                    xAxisTitle              = "P-value",
                                                    yAxisTitle              = "Count",
                                                    hideHistogram           = FALSE,
                                                    showHistogramDensity    = FALSE,
                                                    showLegend              = FALSE,
-                                                   segregateVariablesBy    = list("Levels"))
+                                                   segregateVariablesBy    = list("levels"))
         } else {
-            for (i in 1:NumSamples) {
-                s <- SampNames[i]
-                MyPVal <- dplyr::filter(P.Val, grepl(s, Levels))
-                cx.data <- subset(MyPVal, select = Pval)
-                var.annot <- subset(MyPVal, select = -c(Pval))
-                Hist_Pval <- canvasXpress::canvasXpress(data                    = cx.data,
+            for (i in 1:samples_num) {
+                s <- samples_name[i]
+                pval_subset <- dplyr::filter(P.Val, grepl(s, levels))
+                cx.data <- subset(pval_subset, select = pval)
+                var.annot <- subset(pval_subset, select = -c(pval))
+                hist_pval <- canvasXpress::canvasXpress(data                    = cx.data,
                                                         varAnnot                = var.annot,
                                                         histogramData           = TRUE,
                                                         graphType               = "Scatter2D",
@@ -93,18 +128,17 @@ plotPvalHist <- function(P.Val,
                                                         yAxisTitle              = "Count",
                                                         hideHistogram           = FALSE,
                                                         showHistogramDensity    = FALSE,
-                                                        showLegend              = FALSE,
-                                                        segregateVariablesBy    = list("Levels"))
+                                                        showLegend              = FALSE)
 
-                plotlist[[i]] = Hist_Pval
+                plotlist[[i]] = hist_pval
             }
         }
     } else {
         if (facet) {
             numcol <- 3
-            numrow <- (NumSamples / numcol) %>% ceiling
+            numrow <- (samples_num / numcol) %>% ceiling
 
-            plotlist <- ggplot2::ggplot(data = P.Val, aes(x = Pval)) +
+            plotlist <- ggplot2::ggplot(data = P.Val, aes(x = pval)) +
                 ggplot2::geom_histogram(alpha = alpha,
                                         fill = fill,
                                         color = color,
@@ -113,13 +147,13 @@ plotPvalHist <- function(P.Val,
                 ggplot2::ylab("Count") +
                 ggtitle(title) +
                 ggplot2::scale_fill_brewer(palette = "Set1") +
-                ggplot2::facet_wrap(~Levels, nrow = numrow, scales = "free")
+                ggplot2::facet_wrap(~levels, nrow = numrow, scales = "free")
         } else {
-            for (i in 1:NumSamples) {
-                s <- SampNames[i]
-                MyPVal <- dplyr::filter(P.Val, grepl(s, Levels))
+            for (i in 1:samples_num) {
+                s <- samples_name[i]
+                pval_subset <- dplyr::filter(P.Val, grepl(s, levels))
 
-                Hist_Pval <- ggplot2::ggplot(data = MyPVal, aes(x = Pval)) +
+                hist_pval <- ggplot2::ggplot(data = pval_subset, aes(x = pval)) +
                     ggplot2::geom_histogram(alpha = alpha,
                                             fill = fill,
                                             color = color,
@@ -128,7 +162,7 @@ plotPvalHist <- function(P.Val,
                     ggplot2::ylab("Count") +
                     ggplot2::ggtitle(paste(title, "\n", s))
 
-                plotlist[[i]] = Hist_Pval
+                plotlist[[i]] = hist_pval
             }
         }
     }
